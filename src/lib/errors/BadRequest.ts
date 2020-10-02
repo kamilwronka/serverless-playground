176f2e47ee456044970c10d69450ee5868e8b146
Kamil Wronka <kamil.wronka@gft.com>
2020-10-02 16:19:49 +0200

    Added headers to api

--- src/lib/errors/BadRequest.ts
+++ src/lib/errors/BadRequest.ts
@@ -1,9 +1,14 @@
 export default class BadRequest {
     body: string;
     statusCode: number;
+    headers: any;
 
     constructor(message: any) {
         this.body = JSON.stringify(message);
         this.statusCode = 400;
+        this.headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        };
     }
 }
